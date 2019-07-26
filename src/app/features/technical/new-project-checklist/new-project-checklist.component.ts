471f8f2e7661a6be8054f75f55d4adeff4b8ec6e
Phodal HUANG <h@phodal.com>
2019-07-26 14:05:13 +0800

    feat: make first json init

--- src/app/features/technical/new-project-checklist/new-project-checklist.component.ts
+++ src/app/features/technical/new-project-checklist/new-project-checklist.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import TECH_CHECKLIST from '../../../../assets/data/checklists/technology.json';
+import DOMAIN_CHECKLIST from '../../../../assets/data/checklists/domain.json';
+import PROJECT_CHECKLIST from '../../../../assets/data/checklists/process.json';
+import PEOPLE_CHECKLIST from '../../../../assets/data/checklists/people.json';
 
 @Component({
   selector: 'feature-new-project-checklist',
@@ -6,11 +10,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./new-project-checklist.component.scss']
 })
 export class NewProjectChecklistComponent implements OnInit {
-  newProjectChecklist: any;
+  techChecklist: any[];
 
   constructor() { }
 
   ngOnInit() {
+    this.techChecklist = TECH_CHECKLIST[1].subitems.map((item) => {
+      return {
+        task: item.title
+      };
+    });
   }
 
 }
