c726027d580f444ce6a3b03797bade66badfa6eb
Phodal HUANG <h@phodal.com>
2019-07-29 08:22:39 +0800

    fix: fix some styles

--- src/app/presentation/inception/inception.module.ts
+++ src/app/presentation/inception/inception.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 
-import { CoreModule } from '../../core/core.module';
 import { SharedModule } from '../../shared/shared.module';
 import { InceptionRoutingModule } from './inception-routing.module';
 import { InceptionComponent } from './inception.component';
 import { FeaturesModule } from '../../features/features.module';
 import { InceptionNavBarComponent } from './inception-nav-bar/inception-nav-bar.component';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
   declarations: [
@@ -13,9 +14,10 @@ import { InceptionNavBarComponent } from './inception-nav-bar/inception-nav-bar.
     InceptionNavBarComponent
   ],
   imports: [
-    CoreModule,
-    SharedModule,
     FeaturesModule,
+    SharedModule,
+    FormsModule,
+    CommonModule,
     InceptionRoutingModule
   ],
   bootstrap: [InceptionComponent]
