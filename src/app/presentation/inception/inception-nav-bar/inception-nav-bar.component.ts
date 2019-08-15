0c8d68dc026b6651bfd86d3a3575684f7eb7f129
Phodal HUANG <h@phodal.com>
2019-08-15 16:21:59 +0800

    feat: add navigate by url

--- src/app/presentation/inception/inception-nav-bar/inception-nav-bar.component.ts
+++ src/app/presentation/inception/inception-nav-bar/inception-nav-bar.component.ts
@@ -1,11 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+const Mousetrap = require('mousetrap');
 
 @Component({
   selector: 'inception-nav-bar',
   templateUrl: './inception-nav-bar.component.html',
   styleUrls: ['./inception-nav-bar.component.scss']
 })
-export class InceptionNavBarComponent implements OnInit {
+export class InceptionNavBarComponent implements OnInit, AfterViewInit {
   linksData = [
     {
       link: '/inception/schedule',
@@ -36,11 +39,46 @@ export class InceptionNavBarComponent implements OnInit {
       name: '计划'
     }
   ];
+  private currentRouterIndex = 0;
 
-  constructor() {
+  constructor(private router: Router) {
   }
 
   ngOnInit() {
   }
 
+  ngAfterViewInit(): void {
+    this.linksData.forEach((link, index) => {
+      if (link.link === this.router.url) {
+        this.currentRouterIndex = index;
+      }
+    });
+
+    console.log(this.currentRouterIndex);
+
+    Mousetrap.bind('left', this.moveRouterToLeft.bind(this));
+    Mousetrap.bind('right', this.moveRouterToRight.bind(this));
+  }
+
+  private moveRouterToRight() {
+    this.currentRouterIndex++;
+    if (this.currentRouterIndex >= this.linksData.length) {
+      this.currentRouterIndex = 0;
+    }
+    this.routerToIndex();
+    return true;
+  }
+
+  private moveRouterToLeft() {
+    this.currentRouterIndex--;
+    if (this.currentRouterIndex <= 0) {
+      this.currentRouterIndex = this.linksData.length - 1;
+    }
+    this.routerToIndex();
+    return true;
+  }
+
+  private routerToIndex() {
+    this.router.navigateByUrl(this.linksData[this.currentRouterIndex].link);
+  }
 }
