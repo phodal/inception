2bc359c8754921e77b7bd5e45159722a3de00d31
Phodal HUANG <h@phodal.com>
2019-08-15 15:57:33 +0800

    refactor: change navbar to link data

--- src/app/presentation/inception/inception-nav-bar/inception-nav-bar.component.ts
+++ src/app/presentation/inception/inception-nav-bar/inception-nav-bar.component.ts
@@ -6,8 +6,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./inception-nav-bar.component.scss']
 })
 export class InceptionNavBarComponent implements OnInit {
+  linksData = [
+    {
+      link: '/inception/schedule',
+      name: '日程'
+    },
+    {
+      link: '/inception/vision',
+      name: '远景'
+    },
+    {
+      link: '/inception/positioning',
+      name: '定位'
+    },
+    {
+      link: '/inception/personas',
+      name: '用户'
+    },
+    {
+      link: '/inception/business',
+      name: '业务架构'
+    },
+    {
+      link: '/inception/technical',
+      name: '技术架构'
+    },
+    {
+      link: '/inception/planning',
+      name: '计划'
+    }
+  ];
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
   }
